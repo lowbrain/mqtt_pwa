5454eac9336a1dfabde974a567c75a2a5e949e4e
lowbrain <83059975+lowbrain@users.noreply.github.com>
2024-01-20 11:52:33 +0000

    build pwa

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,42 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: { enabled: true }
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg}']
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'MQTT PWA SAMPLE',
+        short_name: 'PWA',
+        description: 'MQTTクライアントが実装されたPWAのサンプル',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
     }),
   ],
   resolve: {
